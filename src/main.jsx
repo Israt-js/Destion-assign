f83ab231d3873ea5ca70d4ba493d8095428e5522
israt.js <israt8291jahan@gmail.com>
2024-10-14 15:19:01 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Login from './Login';
+import Signup from './Signup';
+import Dashboard from './Dashboard';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login></Login>,
+    children: [
+      {
+        path: "/signup",
+        element: <Signup></Signup>,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard></Dashboard>,
+      },
+    ],
+  },
+]);
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
